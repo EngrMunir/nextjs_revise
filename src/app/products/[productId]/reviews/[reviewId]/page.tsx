20c46bcb5594dadee2977546548504bcfb569278
munir <smsirajulmonir@gmail.com>
2025-08-18 13:05:15 +0600

    nested dynamic route explored

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReviewDetails = async({params}:{params: Promise<{ productId:string ,reviewId: string}>}) => {
+    const { productId, reviewId } = await params;
+    return (
+        <div>
+            <h1>Review {reviewId} for product {productId}</h1>
+        </div>
+    );
+};
+
+export default ReviewDetails;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Products = () => {
     return (
-        <div>
+        <>
             <h1>Product 1</h1>
             <h1>Product 2</h1>
             <h1>Product 3</h1>
             <h1>Product 4</h1>
-        </div>
+        </>
     );
 };
 
